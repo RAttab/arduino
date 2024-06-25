692467ca13ba6c939da63548b531c5a62d98a702
Rémi Attab <remi.attab@gmail.com>
2024-06-25 11:00:45 -0400

    Add generic utilities

---
+++ utils/dbg.h
@@ -0,0 +1,20 @@
+// -----------------------------------------------------------------------------
+// dbg
+// -----------------------------------------------------------------------------
+
+static void dbg_setup(void) { Serial.begin(9600); }
+
+static void dbg_(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    char buffer[256] = {0};
+    int n = vsnprintf(buffer, sizeof(buffer), fmt, args);
+    Serial.write(buffer, n);
+
+    va_end(args);
+}
+
+#define dbg(msg) dbg_(msg "\n")
+#define dbgf(fmt, ...) dbg_(fmt "\n", __VA_ARGS__)
---
+++ utils/motor.h
@@ -0,0 +1,45 @@
+
+// -----------------------------------------------------------------------------
+// motor
+// -----------------------------------------------------------------------------
+
+struct motor
+{
+    struct { unsigned left, right, pwm; } pins;
+    int speed;
+};
+
+enum { motor_speed_cap = 255 };
+
+static void motor_setup(
+        struct motor *motor, unsigned right, unsigned left, unsigned pwm)
+{
+    pinMode(motor->pins.right = right, OUTPUT);
+    pinMode(motor->pins.left = left, OUTPUT);
+    pinMode(motor->pins.pwm = pwm, OUTPUT);
+    motor->speed = 0;
+}
+
+static void motor_set(struct motor *motor, int speed)
+{
+    motor->speed = constrain(speed, -motor_speed_cap, motor_speed_cap);
+
+    if (!motor->speed) {
+        digitalWrite(motor->pins.left, 0);
+        digitalWrite(motor->pins.right, 0);
+        digitalWrite(motor->pins.pwm, abs(0));
+        return;
+    }
+
+    if (motor->speed < 0) {
+        digitalWrite(motor->pins.left, 0);
+        digitalWrite(motor->pins.right, 255);
+    }
+    else {
+        digitalWrite(motor->pins.left, 255);
+        digitalWrite(motor->pins.right, 0);
+    }
+
+    digitalWrite(motor->pins.pwm, abs(motor->speed));
+}
+
